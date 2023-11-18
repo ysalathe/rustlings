da0e55e47aea49fd1adbdc18fa84bcc087163d91
Yves Salathe <ysalath@gmail.com>
2023-11-18 22:20:14 +0000

    Add solutions so far

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(qty: i32) -> i32 {
+    let cost_per_apple: i32 = if (qty > 40) { 1 } else { 2 };
+    qty * cost_per_apple
+}
 
 // Don't modify this function!
 #[test]
